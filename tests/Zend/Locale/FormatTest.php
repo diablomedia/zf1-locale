8a6beba9d4422526fb0034f9b08afda8e049b8d6
Jay Klehr <jay@diablomedia.com>
2021-03-06 10:07:20 -0700

    fixing a couple of broken tests

--- tests/Zend/Locale/FormatTest.php
+++ tests/Zend/Locale/FormatTest.php
@@ -658,7 +658,7 @@ class Zend_Locale_FormatTest extends PHPUnit\Framework\TestCase
 
         $this->assertFalse(Zend_Locale_Format::checkDateFormat('20.April', array('date_format' => 'dd.MMMM.YYYY')));
         $this->assertTrue(Zend_Locale_Format::checkDateFormat('20.April', array('date_format' => 'MMMM.YYYY'   )));
-        $this->assertTrue(Zend_Locale_Format::checkDateFormat('20', array('date_format' => 'dd.MMMM.YYYY')));
+        $this->assertFalse(Zend_Locale_Format::checkDateFormat('20', array('date_format' => 'dd.MMMM.YYYY')));
         $this->assertTrue(Zend_Locale_Format::checkDateFormat('April.2007', array('date_format' => 'MMMM.YYYY'   )));
         $this->assertTrue(Zend_Locale_Format::checkDateFormat('20.April.2007', array('date_format' => 'dd.YYYY'     )));
 
@@ -679,7 +679,7 @@ class Zend_Locale_FormatTest extends PHPUnit\Framework\TestCase
         $this->assertTrue(Zend_Locale_Format::checkDateFormat('11:10:55 am', array('date_format' => 'HH:mm:ss', 'locale' => 'ar_EG')));
         $this->assertFalse(Zend_Locale_Format::checkDateFormat('notime'));
         $this->assertFalse(Zend_Locale_Format::checkDateFormat('13:10', array('date_format' => 'HH:mm:ss', 'locale' => 'de_AT')));
-        $this->assertTrue(Zend_Locale_Format::checkDateFormat('13', array('date_format' => 'HH:mm',    'locale' => 'de_AT')));
+        $this->assertFalse(Zend_Locale_Format::checkDateFormat('13', array('date_format' => 'HH:mm',    'locale' => 'de_AT')));
         $this->assertFalse(Zend_Locale_Format::checkDateFormat('00:13', array('date_format' => 'ss:mm:HH', 'locale' => 'de_AT')));
     }
 
