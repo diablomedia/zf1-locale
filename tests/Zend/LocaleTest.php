fe29bad47cc85686d15ae5fa73e8013a9ca2c348
Jay Klehr <jay@diablomedia.com>
2021-03-06 09:44:30 -0700

    fixing error handler for php 8

--- tests/Zend/LocaleTest.php
+++ tests/Zend/LocaleTest.php
@@ -952,7 +952,7 @@ class Zend_LocaleTest extends PHPUnit\Framework\TestCase
      * @param  array   $errcontext
      * @return void
      */
-    public function errorHandlerIgnore($errno, $errstr, $errfile, $errline, array $errcontext)
+    public function errorHandlerIgnore($errno, $errstr, $errfile, $errline, array $errcontext = array())
     {
         $this->_errorOccurred = true;
     }
